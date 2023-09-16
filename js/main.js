0636359ccbcf0208a5c12cf195e468a6a0f1f4d0
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-15 23:51:18 -0300

    feat: creating provisional drop down logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-const hamburguer = document.getElementById("hamburguer");
+const hamburguer = document.querySelector("#hamburguer");
+const productToggle = document.querySelector("#product-toggle");
+const companyToggle = document.querySelector("#company-toggle");
+const connectToggle = document.querySelector("#connect-toggle");
+const productDrop = document.querySelector("#product-dropdown");
+const companyDrop = document.querySelector("#company-dropdown");
+const connectDrop = document.querySelector("#connect-dropdown");
 
 hamburguer.addEventListener("click", hideMenu);
 
 function hideMenu() {
   let button = document.getElementById("hamburguer-menu");
 
-  if (button.src.match("../public/icon-hamburger.svg")) {
-    button.src = "../public/icon-close.svg";
-  } else {
-    button.src = "../public/icon-hamburger.svg";
-  }
+  // prettier-ignore
+  if (button.src.match("/icon-hamburger.svg")) return (button.src = "/icon-close.svg");
+  return (button.src = "/icon-hamburger.svg");
 }
+
+function dropNavigation(menu, nav) {
+  menu.addEventListener("click", () => {
+    // nav.classList.remove("hide");
+    if (nav.classList.contains("hide")) return nav.classList.remove("hide");
+    return nav.classList.add("hide");
+  });
+}
+
+function menuDrop() {
+  dropNavigation(productToggle, productDrop);
+  dropNavigation(companyToggle, companyDrop);
+  dropNavigation(connectToggle, connectDrop);
+}
+
+menuDrop();
